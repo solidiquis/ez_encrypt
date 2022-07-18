41611239534977487151c0bd4a3329736e70b527
Benji Nguyen <benjamin.van.nguyen@gmail.com>
2022-07-17 19:11:28 -0600

    logging

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,17 @@
 use actix_files as fs;
 use actix_web::{App, HttpServer, HttpRequest, web, Result};
+use simple_logger::SimpleLogger;
+use std::str::FromStr;
 use std::path::PathBuf;
 
 const STATIC_ASSETS_PATH: &'static str = "../client/dist";
 
-async fn index(_req: HttpRequest) -> Result<fs::NamedFile> {
+async fn index(req: HttpRequest) -> Result<fs::NamedFile> {
+    let conn_info = req.connection_info();
+    if let Some(ip) = conn_info.realip_remote_addr() {
+        log::info!("ip={ip}");
+    }
+
     let file_path: PathBuf = [STATIC_ASSETS_PATH, "index.html"].iter().collect();
     Ok(fs::NamedFile::open(file_path)?)
 }
@@ -16,8 +23,26 @@ fn config(cfg: &mut web::ServiceConfig) {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
+    SimpleLogger::new().with_level(log::LevelFilter::Info).init().unwrap();
+
+    let host = if let Ok(h) = dotenv::var("HOST") {
+        h
+    } else {
+        "127.0.0.1".to_string()
+    };
+
+    let port = if let Ok(ref p) = dotenv::var("PORT") {
+        u16::from_str(p).unwrap()
+    } else {
+        3000
+    };
+
+    log::info!("Listening on tcp://{}:{}", &host, &port);
+
     HttpServer::new(|| App::new().configure(config))
-        .bind(("127.0.0.1", 3000))?
+        .bind((host, port))?
         .run()
         .await
 }
